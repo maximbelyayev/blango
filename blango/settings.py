9f10e691b83a96978678bd6165bd70faeb98e165
M B <maximbelyayev@gmail.com>
2024-08-17 15:11:52 +0000

    Finish crispy forms

--- blango/settings.py
+++ blango/settings.py
@@ -45,8 +45,21 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog', 
+    
+    'crispy_forms',
+    'crispy_bootstrap5',
+
+    'blog',
+    
 ]
+
+# App settings
+
+## Crispy forms
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
+
+# Middleware
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
